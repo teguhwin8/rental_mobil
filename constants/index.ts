8b227bc97867c78ac80a0663a955ca458ca37e8f
Teguh Widodo <teguhwin8@gmail.com>
2023-06-18 01:41:26 +0700

    add a new component Footer

---
+++ constants/index.ts
@@ -0,0 +1,101 @@
+export const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
+
+export const yearsOfProduction = [
+  { title: "Year", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" },
+  { title: "2020", value: "2020" },
+  { title: "2021", value: "2021" },
+  { title: "2022", value: "2022" },
+  { title: "2023", value: "2023" },
+];
+
+export const fuels = [
+  {
+    title: "Bensin",
+    value: "",
+  },
+  {
+    title: "Solar",
+    value: "Solar",
+  },
+  {
+    title: "Mobil Listrik",
+    value: "Mobil Listrik",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Tentang Kami",
+    links: [
+      { title: "Cara Kerja", url: "/" },
+      { title: "Fitur", url: "/" },
+      { title: "Kerjasama", url: "/" },
+      { title: "Lowongan Kerja", url: "/" },
+    ],
+  },
+  {
+    title: "Perusahaan",
+    links: [
+      { title: "Event", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Ajak Teman", url: "/" },
+    ],
+  },
+  {
+    title: "Sosial Media",
+    links: [
+      { title: "Discord", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
