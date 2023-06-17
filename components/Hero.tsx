256ffc2fd9f9c2d7b0be3ee75810d500b18c3b4f
Teguh Widodo <teguhwin8@gmail.com>
2023-06-18 00:59:28 +0700

    added Hero and CustomButton components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image"
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const handleScroll = () => {};
 
-export default Hero
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Temukan, Cari, dan Rental Mobil - Mudah dan Cepat
+        </h1>
+        <p className="hero__subtitle">
+          Nikmati pengalaman rental mobil Anti Ribet dengan harga terjangkau
+        </p>
+        <CustomButton
+          title="Jelajahi Sekarang"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="Hero images"
+            fill
+            className="object-contain"
+          />
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
