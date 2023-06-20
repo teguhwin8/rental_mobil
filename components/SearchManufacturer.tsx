39efa47dd9dfab91ffd828069d43e3a58e20d26e
Teguh Widodo <teguhwin8@gmail.com>
2023-06-21 00:04:36 +0700

    search manufacturer with some data

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { useState, Fragment } from "react";
+import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 
-import { useState, Fragment } from "react";
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
-import Image from "next/image";
 
 const SearchManufacturer = ({
   manufacturer,
@@ -12,11 +13,21 @@ const SearchManufacturer = ({
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
 
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
-          <Combobox.Button className="absolute top-[14px]">
+          <Combobox.Button className="absolute top-[14px] left-3">
             <Image src="/car-logo.svg" alt="Car Logo" width={20} height={20} />
           </Combobox.Button>
           <Combobox.Input
@@ -32,7 +43,31 @@ const SearchManufacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options></Combobox.Options>
+            <Combobox.Options className="mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-bold' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                      </>
+                    )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
