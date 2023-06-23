e0e363c586c91149408e5f930cc3bcb6429dbc5b
Teguh Widodo <teguhwin8@gmail.com>
2023-06-24 00:20:33 +0700

    fixing searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,17 +5,15 @@ import SearchManufacturer from "./SearchManufacturer";
 import Image from "next/image";
 
 const SearchButton = ({ otherClass }: { otherClass: string }) => (
-  <>
-    <button type="submit" className={`-ml-11 z-0 ${otherClass}`}>
-      <Image
-        src="/magnifying-glass.svg"
-        width={40}
-        height={40}
-        alt="magnifying glass"
-        className="object-contain"
-      />
-    </button>{" "}
-  </>
+  <button type="submit" className={`-ml-11 z-0 ${otherClass}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      width={40}
+      height={40}
+      alt="magnifying glass"
+      className="object-contain"
+    />
+  </button>
 );
 
 const SearchBar = () => {
@@ -25,36 +23,34 @@ const SearchBar = () => {
   const handleSearch = () => {};
 
   return (
-    <div>
-      <form className="searchbar" onSubmit={handleSearch}>
-        <div className="searchbar__item">
-          <SearchManufacturer
-            manufacturer={manufacturer}
-            setManufacturer={setManufacturer}
-          />
-          <SearchButton otherClass="sm:hidden" />
-        </div>
-        <div className="searchbar__item">
-          <Image
-            src="/model-icon.png"
-            alt="car model"
-            width={25}
-            height={25}
-            className="absolute w-[20px] h-[20px] ml-4"
-          />
-          <input
-            type="text"
-            name="model"
-            value={model}
-            placeholder="Model"
-            className="searchbar__input"
-            onChange={(e) => setModel(e.target.value)}
-          />
-          <SearchButton otherClass="sm:hidden" />
-        </div>
-        <SearchButton otherClass="max-sm:hidden" />
-      </form>
-    </div>
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClass="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="car model"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          placeholder="Model"
+          className="searchbar__input"
+          onChange={(e) => setModel(e.target.value)}
+        />
+        <SearchButton otherClass="sm:hidden" />
+      </div>
+      <SearchButton otherClass="max-sm:hidden" />
+    </form>
   );
 };
 
